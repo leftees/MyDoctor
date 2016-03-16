08966efc813ba6a52be3c3a4e57927f0fdf580c1
sheryl_john <sherylmariamjohn@gmail.com>
2016-03-16 17:20:26 +0530

    feature specs for doctor categories

---
+++ spec/features/doctor_categories/edit_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature 'Edit Doctor Category' do
+
+  scenario 'Edit doctor category' do
+  	dc = FactoryGirl.create(:doctor_category)
+    visit "doctor_categories/#{dc.id}/edit"
+    fill_in "Name", :with => "My test name"
+    click_button "Update Doctor category"
+    expect(page).to have_content 'doctor category was successfully updated.'
+  end
+
+  scenario 'Edit doctor category with blank name' do
+  	dc = FactoryGirl.create(:doctor_category)
+    visit "doctor_categories/#{dc.id}/edit"
+    fill_in "Name", :with => ""
+    click_button "Update Doctor category"
+    expect(page).to have_content 'Editing doctor'
+  end
+
+
+  scenario 'Edit doctor category with alpha numeric letters' do
+  	dc = FactoryGirl.create(:doctor_category)
+    visit "doctor_categories/#{dc.id}/edit"
+    fill_in "Name", :with => "45565**%$^"
+    click_button "Update Doctor category"
+    expect(page).to have_content 'Editing doctor'
+  end
+end
---
+++ spec/features/doctor_categories/show_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature 'Show Doctor Category' do
+
+  scenario 'Show doctor category' do
+  	dc = FactoryGirl.create(:doctor_category)
+    visit "doctor_categories/#{dc.id}"
+    
+    expect(page).to have_content dc.name
+  end
+end
