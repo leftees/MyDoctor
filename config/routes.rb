799f8c471d93c801fb00ac0a27538cb93ffce66f
mathewvivek <mathewvivek>
2016-03-16 17:12:59 +0530

    Changed Route for RailsAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :patients, :path_names => {:sign_up => "sign_up"},:controllers => {:registrations => "patients/registrations"}
   devise_for :doctors, :path_names => {:sign_up => "sign_up"},:controllers => {:registrations => "doctors/registrations"}
+  resources :doctors
   root to: 'visitors#index'
   devise_for :users
   resources :users
