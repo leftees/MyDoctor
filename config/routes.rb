8310b84f3556a7058012eb8bb97678ccf6d7a21d
mathewvivek <mathewvivek>
2016-03-16 14:41:46 +0530

    Doctor Sign Up Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :patients
-  devise_for :doctors
+  devise_for :patients, :path_names => {:sign_up => "sign_up"},:controllers => {:registrations => "patients/registrations"}
+  devise_for :doctors, :path_names => {:sign_up => "sign_up"},:controllers => {:registrations => "doctors/registrations"}
   root to: 'visitors#index'
   devise_for :users
   resources :users
