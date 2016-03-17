a3cdb3478ab14ad375440397d976f4571eb48e6f
sheryl_john <sherylmariamjohn@gmail.com>
2016-03-17 14:36:55 +0530

    fix spec issues

--- spec/features/doctor_categories/create_spec.rb
+++ spec/features/doctor_categories/create_spec.rb
@@ -5,21 +5,21 @@ feature 'Create Doctor Category' do
 
   scenario 'create new doctor categories with valid name' do
     visit 'doctor_categories/new'
-    fill_in "Name", :with => "My test name"
+    fill_in "doctor_category_name", :with => "My test name"
     click_button "Create Doctor category"
     expect(page).to have_content 'doctor category uploaded'
   end
 
   scenario 'create new doctor categories with blank name' do
     visit 'doctor_categories/new'
-    fill_in "Name", :with => ""
+    fill_in "doctor_category_name", :with => ""
     click_button "Create Doctor category"
     expect(page).to have_content 'New Doctor Category'
   end
 
   scenario 'create new doctor categories with alpha numeric letters' do
     visit 'doctor_categories/new'
-    fill_in "Name", :with => "423434**^^^"
+    fill_in "doctor_category_name", :with => "423434**^^^"
     click_button "Create Doctor category"
     expect(page).to have_content 'New Doctor Category'
   end
--- spec/features/doctor_categories/edit_spec.rb
+++ spec/features/doctor_categories/edit_spec.rb
@@ -6,7 +6,7 @@ feature 'Edit Doctor Category' do
   scenario 'Edit doctor category from index page' do
   	dc = FactoryGirl.create(:doctor_category)
     visit "doctor_categories/#{dc.id}/edit"
-    fill_in "Name", :with => "My test name"
+    fill_in "doctor_category_name", :with => "My test name"
     click_button "Update Doctor category"
     expect(page).to have_content 'doctor category was successfully updated.'
   end
@@ -14,7 +14,7 @@ feature 'Edit Doctor Category' do
   scenario 'Edit doctor category with blank name' do
   	dc = FactoryGirl.create(:doctor_category)
     visit "doctor_categories/#{dc.id}/edit"
-    fill_in "Name", :with => ""
+    fill_in "doctor_category_name", :with => ""
     click_button "Update Doctor category"
     expect(page).to have_content 'Editing Doctor'
   end
@@ -23,7 +23,7 @@ feature 'Edit Doctor Category' do
   scenario 'Edit doctor category with alpha numeric letters' do
   	dc = FactoryGirl.create(:doctor_category)
     visit "doctor_categories/#{dc.id}/edit"
-    fill_in "Name", :with => "45565**%$^"
+    fill_in "doctor_category_name", :with => "45565**%$^"
     click_button "Update Doctor category"
     expect(page).to have_content 'Editing Doctor'
   end
