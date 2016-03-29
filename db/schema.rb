9373f1fb0df49c543e5bc42b3cd4658c65b107f7
Jijo Thomas <jijo487@gmail.com>
2016-03-29 11:21:52 +0530

    committing new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318063411) do
+ActiveRecord::Schema.define(version: 20160328093433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "doccuments", force: :cascade do |t|
-    t.integer  "doctor_id"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-  end
 
   create_table "doctor_categories", force: :cascade do |t|
     t.string   "name",       default: "", null: false
@@ -67,6 +57,16 @@ ActiveRecord::Schema.define(version: 20160318063411) do
   add_index "doctors", ["confirmation_token"], name: "index_doctors_on_confirmation_token", unique: true, using: :btree
   add_index "doctors", ["email"], name: "index_doctors_on_email", unique: true, using: :btree
   add_index "doctors", ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true, using: :btree
+
+  create_table "documents", force: :cascade do |t|
+    t.integer  "doctor_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
