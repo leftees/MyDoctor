8de7f6264a687c49358f87a5247dd5e52dab8229
mathewvivek <mathewvivek>
2016-04-06 17:13:59 +0530

    dp for doctor category

--- spec/features/doctor_categories/create_spec.rb
+++ spec/features/doctor_categories/create_spec.rb
@@ -6,21 +6,21 @@ feature 'Create Doctor Category' do
   scenario 'create new doctor categories with valid name' do
     visit 'doctor_categories/new'
     fill_in "doctor_category_name", :with => "My test name"
-    click_button "Create Doctor category"
+    click_button "Submit"
     expect(page).to have_content 'doctor category uploaded'
   end
 
   scenario 'create new doctor categories with blank name' do
     visit 'doctor_categories/new'
     fill_in "doctor_category_name", :with => ""
-    click_button "Create Doctor category"
+    click_button "Submit"
     expect(page).to have_content 'New Doctor Category'
   end
 
   scenario 'create new doctor categories with alpha numeric letters' do
     visit 'doctor_categories/new'
     fill_in "doctor_category_name", :with => "423434**^^^"
-    click_button "Create Doctor category"
+    click_button "Submit"
     expect(page).to have_content 'New Doctor Category'
   end
 
