1da56520533423ec0708549d2ee54daf7e785fa0
mathewvivek <mathewvivek>
2016-04-06 15:38:59 +0530

    Doctor Patient Display Picture Paperclip

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,4 +5,7 @@ class Patient < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          validates :name, :age, :phone_number, presence: true
+
+  has_attached_file :avatar, styles: { large: "520x520>", medium: "300x300>", small: "256x256>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
