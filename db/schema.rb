4098077ec86bdeb5c509e74315ef3ac10f9d61c5
Jijo Thomas <jijo487@gmail.com>
2016-04-07 14:32:57 +0530

    adding new schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406110645) do
+ActiveRecord::Schema.define(version: 20160407084112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,18 +114,6 @@ ActiveRecord::Schema.define(version: 20160406110645) do
   add_index "patients", ["confirmation_token"], name: "index_patients_on_confirmation_token", unique: true, using: :btree
   add_index "patients", ["email"], name: "index_patients_on_email", unique: true, using: :btree
   add_index "patients", ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true, using: :btree
-
-  create_table "pictures", force: :cascade do |t|
-    t.integer  "imageable_id"
-    t.string   "imageable_type"
-    t.string   "name"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
