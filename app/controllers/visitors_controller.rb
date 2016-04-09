b9ae5c58b80f9238862d656457febb7be95e5479
Manoj Menon <mhmenon910@gmail.com>
2016-04-09 21:58:38 +0530

    Just a simple indentation change

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,7 @@
 class VisitorsController < ApplicationController
-
-	def index
-		if doctor_signed_in?
-			@doctor = Doctor.find(current_doctor.id)
-		end
-	end
-
+  def index
+    if doctor_signed_in?
+      @doctor = Doctor.find(current_doctor.id)
+    end
+  end
 end
