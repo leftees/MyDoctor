7a85e152348d6a8bfc80bf4f0bb2d679a728d6a7
Jijo Thomas <jijo487@gmail.com>
2016-04-11 17:59:54 +0530

    modifying rescue in create method.

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -4,19 +4,20 @@ class TimeSlotsController < ApplicationController
 
 
   def create
-    begin
+    begin 
       @time_slot = TimeSlot.new(time_slot_params)
       @time_slot.week_days = params[:week_days] 
       @message = "Please check the details" if !@time_slot.save
-    rescue Exception => e 
+    rescue StandardError => e
+      logger.error e.message
     end
     set_time_slots
-  	render  partial: "time_slots"
+  	render partial: "time_slots"
   end
 
   def destroy
     @time_slot.destroy
-    render  partial: "time_slots"
+    render partial: "time_slots"
   end
 
   private
@@ -32,5 +33,5 @@ class TimeSlotsController < ApplicationController
     def time_slot_params
       params.require(:time_slot).permit(:from, :to, :duration,  :doctor_id)
     end
-    
+
 end
